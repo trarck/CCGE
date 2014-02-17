4a920dce550e040ab0f75d2bb06231b3fbe0681d
trarck <trarck@gmail.com>
2014-02-17 10:46:31 +0800

    add animation provider.To parse multiply animation formate

--- Classes/Animations/AnimationProvider.h
+++ Classes/Animations/AnimationProvider.h
@@ -1,38 +1,21 @@
-#ifndef CCGE_CONTROLLERS_FOOTERCONTROLLER_H_
-#define CCGE_CONTROLLERS_FOOTERCONTROLLER_H_
+#ifndef CCGE_ANIMATIONS_ANIMATIONPROVIDER_H_
+#define CCGE_ANIMATIONS_ANIMATIONPROVIDER_H_
 
 #include "yhmvc/Core/LayerController.h"
 #include "yhmvc/Core/Layer.h"
 #include "CCGEMacros.h"
 
 NS_CC_GE_BEGIN
-class FooterController:public yhmvc::LayerController
+class AnimationProvider:public CCObject
 {
 public:
 	
-    FooterController(void);
+    AnimationProvider(void);
     
-	~FooterController(void);
+	~AnimationProvider(void);
 
-	void layerDidLoad();
-
-	void backCallback(CCObject* pSender);
     
-    void exitCallback(CCObject* pSender);
-
-	void showBackButton(){
-		m_backButton->setVisible(true);
-	}
-
-	void hideBackButton(){
-		m_backButton->setVisible(false);
-	}
-
-private:
-
-	CCMenuItemLabel* m_backButton;
-
 };
 NS_CC_GE_END
 
-#endif //CCGE_CONTROLLERS_FOOTERCONTROLLER_H_
+#endif //CCGE_ANIMATIONS_ANIMATIONPROVIDER_H_
