5b9033dbea95541e75baa7f44a44da393cb3b22c
trarck <trarck@gmail.com>
2014-03-21 18:29:38 +0800

    加入地图数据相关逻辑

--- Classes/Services/MissionService.cpp
+++ Classes/Services/MissionService.cpp
@@ -15,4 +15,19 @@ MissionService::~MissionService()
 
 }
 
+int MissionService::getPlayedZone()
+{
+    return 1;
+}
+
+int MissionService::getPlayedMap()
+{
+    return 1;
+}
+
+void MissionService::completeMap(int mapId)
+{
+    
+}
+
 NS_CC_GE_END
--- Classes/Services/MissionService.h
+++ Classes/Services/MissionService.h
@@ -18,7 +18,45 @@ public:
     
     CCDictionary* getMapStepEvents(int mapId);
     
+    /**
+     * @brief 取得完家所在的区
+     */
+    int getPlayedZone();
+    
+    /**
+     * @brief 取得完家所在的地图
+     */
+    int getPlayedMap();
+    
+    /**
+     * @brief 玩家完成一个地图
+     * 计算一个区，下一个地图
+     *
+     * @param mapId 完成的地图id
+     */
+    void completeMap(int mapId);
+    
 public:
+    
+    inline void setCurrentZone(int currentZone)
+    {
+        m_currentZone = currentZone;
+    }
+    
+    inline int getCurrentZone()
+    {
+        return m_currentZone;
+    }
+    
+    inline void setCurrentMap(int currentMap)
+    {
+        m_currentMap = currentMap;
+    }
+    
+    inline int getCurrentMap()
+    {
+        return m_currentMap;
+    }
     
     inline void setLastMapStepIndex(int lastMapStepIndex)
     {
@@ -31,8 +69,18 @@ public:
     }
     
 protected:
+    //当前探索的区域
+    int m_currentZone;
     
+    //当前探索的地图
+    int m_currentMap;
+    
+    //探索的地图所有的位置.支持多个地图一起探索。
+    std::map<int, int> m_exploreMapStep;
+    
+    //当前探索地图的位置。
     int m_lastMapStepIndex;
+    
 };
 
 NS_CC_GE_END
